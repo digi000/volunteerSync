4b04726f702ac3a6906d5bf2b69bbea3f60714b1
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-07-26 00:11:12 +0200

    Create Organization Registratration Form

---
+++ app/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from .models import Organizations
+
+class OrganizationRegistratrationForm(FlaskForm):
+    name = StringField('Organization Name', 
+                      validators=[DataRequired(), Length(max=45)])
+    email = StringField('Email', 
+                       validators=[DataRequired(), Email(), Length(max=255)])
+    address = StringField('Address', 
+                         validators=[DataRequired(), Length(max=45)])
+    telephone = StringField('Phone Number', 
+                           validators=[DataRequired(), Length(min=10, max=10)])
+    password = PasswordField('Password', 
+                            validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField('Confirm Password', 
+                                   validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register Organization')
+
+    def validate_email(self, field):
+        if Organizations.query.filter_by(email=field.data.lower().strip()).first():
+            raise ValidationError('Email already registered')

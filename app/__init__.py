7b8c2c33fd992865269174767d77bc231c52f9f4
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-07-25 23:34:46 +0200

    create home and auth blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from .home.home import home
+from .auth.auth import auth
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/volunteersync'
 
 db = SQLAlchemy(app)
+
+app.register_blueprint(home)
+app.register_blueprint(auth)

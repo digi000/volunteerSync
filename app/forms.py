369464d82853ab7d5513f7c9f43b726c1cad513a
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-07-26 15:18:21 +0200

    create Organization Login Form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 from .models import Organizations
 
@@ -21,3 +21,15 @@ class OrganizationRegistrationForm(FlaskForm):
     def validate_email(self, field):
         if Organizations.query.filter_by(email=field.data.lower().strip()).first():
             raise ValidationError('Email already registered')
+        
+class OrganizationLoginForm(FlaskForm):
+    email = StringField('Email', validators=[
+        DataRequired(),
+        Email(),
+        Length(max=255)
+    ])
+    password = PasswordField('Password', validators=[
+        DataRequired()
+    ])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
